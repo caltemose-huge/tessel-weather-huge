68a49e8d84bffa66a78abff5bfa4366797750c5a
Chad Altemose <caltemose@hugeinc.com>
2019-04-11 13:03:36 -0400

    style enhancements; added avg temp

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,21 +1,33 @@
 Zepto(function ($) {
   $.getJSON('/api', function (data) {
-      drawGraphs(data.records)
+    drawGraphs(data.records)
   })
 
   function drawGraphs (records) {
-      var trace = {
-          x: [],
-          y: [],
-          type: 'scatter'
+    var trace = {
+      x: [],
+      y: [],
+      type: 'scatter'
+    }
+    var totalTemp = 0
+    for(var i=0; i<records.length; i++) {
+      var record = records[i]
+      trace.x.push(record.fields.date)
+      trace.y.push(record.fields.farenheit)
+      totalTemp += Number(record.fields.farenheit)
+    }
+    var data = [trace]
+    var layout = {
+      font: {
+        family: "Georgia, serif",
+        size: 16
       }
-      for(var i=0; i<records.length; i++) {
-          var record = records[i]
-          trace.x.push(record.fields.date)
-          trace.y.push(record.fields.farenheit)
-      }
-      var data = [trace]
-      Plotly.newPlot('graph', data, { responsive: true })
-      $("#loader").hide()
+    }
+    Plotly.newPlot('graph', data, layout, { responsive: true })
+    $("#loader").hide()
+
+    var factsHtml = '<p>Average temperature: ' + Number(totalTemp/records.length).toFixed(2) + '</p>'
+
+    $('#facts').html(factsHtml)
   }
 })
