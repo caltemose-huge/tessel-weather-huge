f0fb460201810c95b8024684f51127c4d2d81237
Chad Altemose <caltemose@hugeinc.com>
2019-04-12 16:02:13 -0400

    removing seemingly erroneous temperatures

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -10,12 +10,20 @@ Zepto(function ($) {
       mode: 'markers'
     }
     var totalTemp = 0
+    var record
     for(var i=0; i<records.length; i++) {
-      var record = records[i]
-      trace.x.push(record.fields.date)
-      trace.y.push(record.fields.farenheit)
+      record = records[i]
       totalTemp += Number(record.fields.farenheit)
     }
+    var avg = Number(totalTemp/records.length).toFixed(2)
+    for(var i=0; i<records.length; i++) {
+      record = records[i]
+      if (Math.abs(avg - record.fields.farenheit) < 20) {
+        trace.x.push(record.fields.date)
+        trace.y.push(record.fields.farenheit)
+      }
+    }
+
     var data = [trace]
     var layout = {
       font: {
